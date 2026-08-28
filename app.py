782298bb4a5e28378a4db1a7c5fb017e7259f608
bungamiftahul14 <bungamiftahul.14@gmail.com>
2026-08-28 15:52:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -180,7 +180,7 @@ with tab1:
     st.divider()
 
     # 2. Ranking Total Pergerakan Bus 17 Terminal & Anomali Manggarai
-    st.subheader("🚌 Evaluasi Lalu Lintas Pergerakan Bus (17 Terminal)")
+    st.subheader(" Evaluasi Lalu Lintas Pergerakan Bus (17 Terminal)")
     
     bus_ranking = df.groupby('terminal').agg({
         'jumlah_bus_datang': 'sum',
